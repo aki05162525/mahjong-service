2ea49f3a0167b042a22eb0c36443cca2a4537a94
aki05162525 <akihiro05162525@gmail.com>
2025-05-11 13:26:34 +0900

    first commit

---
+++ src/main/java/com/kojirou/mahjongservice/MahjongServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kojirou.mahjongservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahjongServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahjongServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kojirou/mahjongservice/MahjongServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kojirou.mahjongservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahjongServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
